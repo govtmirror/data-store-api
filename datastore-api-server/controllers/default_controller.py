8a3baef84d550b115408c125572664cb63efc4c5
Arman Frasier <armanfrasier@gmail.com>
2016-05-09 10:33:21 -0400

    YAML Update + Server Stub
    Stub server up, moving on to implementing mock data

---
+++ datastore-api-server/controllers/default_controller.py
@@ -0,0 +1,12 @@
+
+def awards_fain_fain_get(fAIN):
+    return 'do some magic!'
+
+def awards_piid_piid_get(pIID):
+    return 'do some magic!'
+
+def awards_uri_uri_get(uRI):
+    return 'do some magic!'
+
+def awardsqualifiers_get(qualifiers):
+    return 'do some magic!'
