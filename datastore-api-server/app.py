6ec0ace4e5a0735e8e6315608db5c304858367a3
Arman Frasier <armanfrasier@gmail.com>
2016-05-09 11:11:23 -0400

    Updating Mock responses
    Updating mock responses - moved responses into results attribute of the
    returned object due to security issue in flask.jsonify

--- datastore-api-server/app.py
+++ datastore-api-server/app.py
@@ -5,4 +5,5 @@ import connexion
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.add_api('swagger.yaml', arguments={'title': 'Data Act Datastore API'})
+    # app.debug = True
     app.run(port=8080)
